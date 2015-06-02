ce697b23f8081c872460544678337c650290ed26
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2015-06-02 18:52:46 +0900

    コメント修正

--- sample01-nodejs.js
+++ sample01-nodejs.js
@@ -3,7 +3,7 @@
  * 都道府県コード(pref)を取得してから、市町村コード(city)で取得する
  *
  * 引数に 都道府県コード を指定する
- * e.g. node ssample01-nodejs.js tokyo
+ * e.g. node sample01-nodejs.js tokyo
  * [都道府県コード](https://gist.githubusercontent.com/tatsuyano/82f6b59b050e500d8947/raw/e2ba56c185ff7dd9ef7919412f96aba85d4a57c4/pref)
  * [市区町村コード](https://gist.githubusercontent.com/tatsuyano/2a6b9e8170417025d74e/raw/489c42c171ed1127d6ef368b2d066647b8649d89/city)
  *
