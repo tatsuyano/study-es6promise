13736f5bbe3f4d90514c30ed028c96341ba463d7
Tatsuya NOGUCHI <tatsuyano@gmail.com>
2015-06-02 19:12:06 +0900

    コメントをREADME.mdに移動

--- sample01-nodejs.js
+++ sample01-nodejs.js
@@ -1,14 +1,4 @@
 /*
- * es6-promiseを使って、非同期処理を指定した順番に実行するサンプル
- * 都道府県コード(pref)を取得してから、市町村コード(city)で取得する
- *
- * 引数に 都道府県コード を指定する
- * e.g. node sample01-nodejs.js tokyo
- * [都道府県コード](https://gist.githubusercontent.com/tatsuyano/82f6b59b050e500d8947/raw/e2ba56c185ff7dd9ef7919412f96aba85d4a57c4/pref)
- * [市区町村コード](https://gist.githubusercontent.com/tatsuyano/2a6b9e8170417025d74e/raw/489c42c171ed1127d6ef368b2d066647b8649d89/city)
- *
- */
-
 'use strict'
 
 var Promise = require('es6-promise').Promise;
